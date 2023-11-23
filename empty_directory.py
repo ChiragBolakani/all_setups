30613069cf7d231c53423bd8d207809040878b55
ChiragBolakani <cheragbolakani@gmail.com>
2023-11-23 16:59:04 +0530

    remove setup csv

--- empty_directory.py
+++ empty_directory.py
@@ -19,3 +19,12 @@ os.mkdir("setup_data_column_encoded")
 shutil.rmtree("setup_json_data")
 os.mkdir("setup_json_data")
 
+if os.path.exists("accuracy_bar_plot.png"):
+    os.remove("accuracy_bar_plot.png")
+else:
+    pass
+
+if os.path.exists("setup.csv"):
+    os.remove("setup.csv")
+else:
+    pass
