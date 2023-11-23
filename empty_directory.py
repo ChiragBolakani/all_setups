515d019ffcede134a27f750b5008c4c4c2c43cd6
ChiragBolakani <cheragbolakani@gmail.com>
2023-11-23 16:52:55 +0530

    added new folder

--- empty_directory.py
+++ empty_directory.py
@@ -4,11 +4,18 @@ import shutil
 shutil.rmtree("setup_columns_extracted_data")
 os.mkdir("setup_columns_extracted_data")
 
-shutil.rmtree("setup_data")
-os.mkdir("setup_data")
+shutil.rmtree("setup_columns_extracted_data_with_reason")
+os.mkdir("setup_columns_extracted_data_with_reason")
+
+shutil.rmtree("setup_data_column_vectorized")
+os.mkdir("setup_data_column_vectorized")
+
+# shutil.rmtree("setup_data")
+# os.mkdir("setup_data")
 
 shutil.rmtree("setup_data_column_encoded")
 os.mkdir("setup_data_column_encoded")
 
 shutil.rmtree("setup_json_data")
 os.mkdir("setup_json_data")
+
