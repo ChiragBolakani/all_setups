4724148acf9a3d6a60a1ecd2ab01eb4b3101e784
ChiragBolakani <cheragbolakani@gmail.com>
2023-11-24 11:19:10 +0530

    added commands bat file

---
+++ commands.bat
@@ -0,0 +1,9 @@
+python get_setup_names.py
+python get_setup_data_json.py
+python get_input_data.py
+python label_encoding.py
+python fit_knn.py
+python fit_dct.py
+python fit_svc.py
+python result_analysis.py
+python empty_directory.py
